255621043176c0f81fb7568d3a6f66649d95bc94
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-16 10:32:27 +0300

    Update movie_rate.py

--- movies_rate.py
+++ movies_rate.py
@@ -1,7 +1,7 @@
 movie_name = 'iron man'
 rate = 3
 popularity_score = 72.65
-if rate == 4 or popularity_score > 80:
+if rate >= 4 or popularity_score > 80:
     print("Highly recommended") 
 elif rate == 3 or popularity_score > 70:
     print("I recommended it . It is good")     

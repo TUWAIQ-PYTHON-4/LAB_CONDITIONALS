32dd2aa96f6c0f2a52c0e8d2717951c96bb62941
Waleedoff <77509215+Waleedoff@users.noreply.github.com>
2022-05-16 10:12:17 +0300

    Added my_task1

---
+++ main.py
@@ -0,0 +1,16 @@
+movie:str = "red notice"
+rate:int = 3
+score:float = 72.65
+
+
+
+# CONDITIONS.. 
+if rate ==4  or rate > score == 80:
+    print('Highly Recommanded')
+elif rate == 3 or rate > score == 70:
+    print('i recommanded . its good ')
+elif rate == 2 or rate < score == 60:
+    print('you should check it out ')
+else:
+    print('Dont watch it is waste of time ')
+

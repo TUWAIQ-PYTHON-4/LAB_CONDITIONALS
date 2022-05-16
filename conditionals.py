bced8030de4083a95ac1084ee3eeefa6dbdcae98
mhnd <mhnd@hno.com>
2022-05-16 10:32:55 +0300

    add conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,15 @@
+my_movie = "goodman"
+rating_movie = 3
+popularity_score:float= 72.65
+
+if rating_movie >= 4  and popularity_score > 80 :
+    print("Highly recommended")
+elif rating_movie >= 3 and popularity_score > 70 :
+    print("I recommended it . It is good")
+
+if rating_movie <= 2 and popularity_score > 60 :
+    print("You should check it out!")
+    
+elif rating_movie <= 2 and popularity_score >50:
+    print("Don't watch it. It is a waste of time")
+

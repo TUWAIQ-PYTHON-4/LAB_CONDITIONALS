b75a8cfc0a72a355b9915f1bf035c3c3bd2b55ea
Abdulhadi Ahmasani <103156365+Abdulhadi333@users.noreply.github.com>
2022-05-16 10:38:39 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,22 @@
+mmovie_a:int = 5
+
+popularity_score:float = 72.65
+if mmovie_a == 4 and popularity_score > 80:
+    print("Highly recommended")
+
+elif mmovie_a == 3 and popularity_score > 70:
+        print("I recommended it . It is good")
+
+elif mmovie_a == 2 and popularity_score < 60:
+    print("You should check it out!")
+
+else mmovie_a == 2 and  popularity_score < 50:
+    print("Don't watch it. It is a waste of time")
+
+
+
+
+
+
+
+

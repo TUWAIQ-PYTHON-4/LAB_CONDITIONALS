b48c8c156d6e2355a3db98425541900e8be4c010
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-05-16 10:16:22 +0300

    lab 2 for flow control conditionals

---
+++ lab2_conditionals.py
@@ -0,0 +1,15 @@
+
+detective_conan_rating:int = 3
+detective_conan_score:float = 72.65
+
+if detective_conan_rating >= 4 and detective_conan_score > 80 :
+    print('Highly recommended')
+elif detective_conan_rating >= 3 and detective_conan_score > 70 :
+    print('I recommended it . It is good')
+elif detective_conan_rating <= 2 and detective_conan_score > 60 :
+    print('You should check it out!')
+elif detective_conan_rating <= 2 and detective_conan_score < 50 :
+    print('Don not watch it. It is a waste of time') 
+ 
+
+

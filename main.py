ab3b9d04b6cb0248ebf1270fd2e639924b3df1f9
ali <mashbariali@gmail.com>
2022-05-16 10:14:34 +0300

    added naw file and named it

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+Fight_club:int= 3
+
+popularity_score:float=72.65
+
+
+
+if Fight_club >= 4 and popularity_score > 80:
+    print("Highly recommended")
+
+elif Fight_club >= 3 and popularity_score > 70:
+    print("I recommended it . It is good")   
+
+elif Fight_club >=2 and popularity_score > 60:
+    print("You should check it out!")    
+
+else: 
+    print("Don't watch it. It is a waste of time")
+
+
